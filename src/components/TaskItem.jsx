7b1c0c8c9a64ffb06e52801b3e1da8782747cec2
marcos <dev.marcosanjos@gmail.com>
2024-11-29 10:52:54 -0300

    feat: add check and loader icons to TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,16 +1,32 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import LoaderIcon from "../assets/icons/loader-circle.svg?react"
+
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
-    if (task.status === "done")
-      return "bg-[#00ADB5] bg-opacity-10 text-[#00ADB5]"
-    if (task.status === "in_progress")
-      return "bg-[#FFAA04] bg-opacity-10 text-[#ffaa04]"
+    if (task.status === "done") return "bg-[#00ADB5] text-[#00ADB5]"
+    if (task.status === "in_progress") return "bg-[#FFAA04] text-[#ffaa04]"
     if (task.status === "not_started")
       return "bg-[#35383E] bg-opacity-10 text-[#35383e]"
   }
+
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center gap-2 rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()}`}
     >
+      <label
+        className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+      >
+        <input
+          type="checkbox"
+          checked={task.status === "done"}
+          className="absolute h-full w-full cursor-pointer opacity-0"
+        />
+        {task.status === "done" && <CheckIcon />}
+        {task.status === "in_progress" && (
+          <LoaderIcon className="animate-spin" />
+        )}
+      </label>
+
       {task.title}
     </div>
   )
