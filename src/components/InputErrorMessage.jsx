0aa2ea3921c68630d1ba9470c61b305175172886
marcos <dev.marcosanjos@gmail.com>
2024-12-04 22:36:45 -0300

    fix: add PropTypes validation for children in InputErrorMessage component

--- src/components/InputErrorMessage.jsx
+++ src/components/InputErrorMessage.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types"
+
 const InputErrorMessage = ({ children }) => {
   return <p className="text-left text-xs text-brand-danger">{children}</p>
+}
+
+InputErrorMessage.prototype = {
+  children: PropTypes.node.isRequired,
 }
 
 export default InputErrorMessage
