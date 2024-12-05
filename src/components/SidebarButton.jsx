13e6a91550f623aff0bd6ade5f34c472c2ef0b81
marcos <dev.marcosanjos@gmail.com>
2024-12-04 22:19:13 -0300

    refactor: update SidebarButton to use tailwind-variants for styling

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,18 +1,19 @@
-const SidebarButton = ({ children, varient }) => {
-  const getVatientedClasses = () => {
-    if (varient === "default") {
-      return "text-brand-dark-blue"
-    }
+import { tv } from "tailwind-variants"
 
-    if (varient === "selected") {
-      return "text-brand-primary bg-brand-light-gray rounded-lg"
-    }
-  }
+const SidebarButton = ({ children, varient }) => {
+  const sidebar = tv({
+    base: "flex items-center gap-2 px-6 py-2",
+    color: {
+      unselected: "text-brand-dark-blue",
+      selected: "rounded-lg bg-brand-light-gray text-brand-primary",
+    },
+    defaultVariants: {
+      color: "unselected",
+    },
+  })
+
   return (
-    <a
-      href="#"
-      className={`flex items-center gap-2 px-6 py-2 ${getVatientedClasses()}`}
-    >
+    <a href="#" className={sidebar({ varient })}>
       {children}
     </a>
   )
