1da672c7363ec294fc021d592d2df12d8f80d16e
marcos <dev.marcosanjos@gmail.com>
2024-12-04 22:37:09 -0300

    fix: add PropTypes validation for children in InputLabel component

--- src/components/InputLabel.jsx
+++ src/components/InputLabel.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types"
+
 const InputLabel = (props) => {
   return (
     <labe className="text-sm font-semibold text-brand-dark-blue" {...props}>
       {props.children}
     </labe>
   )
+}
+
+InputLabel.prototype = {
+  children: PropTypes.node.isRequired,
 }
 
 export default InputLabel
